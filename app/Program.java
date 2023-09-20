7740dd21f071bab19621ea8ec458bfaad4f173cb
Italo <gabriel.italo.contato@gmail.com>
2023-09-20 13:29:16 -0300

    iniciado o projeto
    Classe UI imprime o jogo, classe jogo tem as regras e construção do jogo, program inicia o app

---
+++ app/Program.java
@@ -0,0 +1,12 @@
+package app;
+
+import entities.Jogo;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Jogo jg = new Jogo();
+	}
+
+}
---
+++ entities/Jogo.java
@@ -0,0 +1,48 @@
+package entities;
+
+public class Jogo {
+	String[][] pecas = new String[3][3];
+
+	public Jogo() {
+		for(int i = 0 ; i < pecas.length; i++) {
+			for(int j = 0; j < pecas[0].length; j++) {
+				pecas[i][j] = "_";
+			}
+		}
+	}
+
+	public void xJoga() {
+
+	}
+
+	public void bJoga() {
+
+	}
+
+	public int empate() {
+		// se der empate retorna 1
+		return 0;
+	}
+
+	public int vitoriaEmLinha() {
+		// se X vencer em linha retorna 2
+		// se nao, se B vencer em linha retorna 3
+		return 0;
+	}
+
+	public int vitoriaEmColuna() {
+		// se X vencer em coluna retorna 2
+		// se nao, se B vencer em coluna retorna 3
+		return 0;
+	}
+
+	public void vitoriaEmDiagonal() {
+		// se X vencer em diagonal retorna 2
+		// se nao, se B vencer em diagonal retorna 3
+	}
+
+	public String[][] getPecas() {
+		return pecas;
+	}
+
+}
