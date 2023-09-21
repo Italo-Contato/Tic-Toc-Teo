d0760254ab2c7bbd4c520d39309d409679d669f3
Italo <gabriel.italo.contato@gmail.com>
2023-09-21 10:57:15 -0300

    Criado metodo comeca() para começar o loop do jogo

--- app/UI.java
+++ app/UI.java
@@ -1,9 +1,33 @@
 package app;
 
+import java.util.Scanner;
+
 import entities.Jogo;
 
 public class UI {
 	public void imprimePecas(Jogo jg) {
 		System.out.println(jg);
 	}
+
+	public void começa(Jogo jg, Scanner sc) {
+		System.out.println("X começa");
+		do {
+
+			if (jg.vitoriaEmColuna() == 0 && jg.vitoriaEmLinha() == 0 && jg.vitoriaEmDiagonal() == 0
+					&& jg.empate() == 0) {
+				System.out.println("Vez do X");
+				jg.xJoga(sc);
+				imprimePecas(jg);
+			}
+
+			if (jg.vitoriaEmColuna() == 0 && jg.vitoriaEmLinha() == 0 && jg.vitoriaEmDiagonal() == 0
+					&& jg.empate() == 0) {
+				System.out.println("Vez do B");
+				jg.bJoga(sc);
+				imprimePecas(jg);
+			}
+		} while (jg.vitoriaEmColuna() == 0 && jg.vitoriaEmLinha() == 0 && jg.vitoriaEmDiagonal() == 0
+				&& jg.empate() == 0);
+	}
+
 }
