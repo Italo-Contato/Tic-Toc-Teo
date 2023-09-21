8c827b6363ae33b264ba97d56d1c8016af8a4586
Italo <gabriel.italo.contato@gmail.com>
2023-09-21 11:26:54 -0300

    Mudança de chamada na classe principal para iniciar o app

--- app/Program.java
+++ app/Program.java
@@ -1,12 +1,18 @@
 package app;
 
+import java.util.Scanner;
+
 import entities.Jogo;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in); 
 		Jogo jg = new Jogo();
+		UI ui = new UI();
+		ui.começa(jg, sc);
+
 	}
 
 }
